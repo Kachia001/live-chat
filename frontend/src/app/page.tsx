6fc744adbc5f2107b61aa3ef46adc2622f5717e1
Kachia001 <kmjok7@naver.com>
2023-11-22 04:15:22 +0900

    add nav-bar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,6 +3,13 @@ import { Content } from './Content';
 export default function Home() {
   return (
     <main className="">
+      <nav className='text-start bg-black text-white p-3 font-bold flex justify-between'>
+        <h3 className='ms-3'>liveChat</h3>
+        <h3 className='me-3'>menu</h3>
+      </nav>
+
+
+
       <Content />
     </main>
   );

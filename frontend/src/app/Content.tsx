23bb300a32df7a921210d880d9b85342ada93d1a
Kachia001 <kmjok7@naver.com>
2023-11-26 04:40:58 +0900

    interface update-remove img

--- frontend/src/app/Content.tsx
+++ frontend/src/app/Content.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { useEffect, useRef, useState } from 'react';
 import { type Socket, io } from 'socket.io-client';
-
+import Image from 'next/image';
+import janimal from './image/janimal.jpg';
+import myungttak from './image/myungttak.jpg';
 export function Content() {
   const socket = useRef<Socket>();
 
@@ -24,6 +26,7 @@ export function Content() {
       client.disconnect();
     };
   }, []);
+
   return (
     <main className="">
       <nav className="text-start bg-black text-white p-3 font-bold flex justify-between">
@@ -36,6 +39,31 @@ export function Content() {
         {!userCount && <div>로딩중입니다.</div>}
         {userCount && <div>{userCount}</div>}
       </div>
+
+      <div className="flex flex-col items-center p-10">
+        <div className="bg-slate-600 h-96 w-full overflow-scroll overflow-x-hidden flex flex-col p-5">
+
+          <div className="text w-full h-fit flex">
+            <div alt='img' className="w-1/12 h-16 bg-slate-50" />
+            {/* <Image src={myungttak} alt='img' className="w-1/12 h-16" /> */}
+            <span className="w-11/12 mx-10">aa</span>
+          </div>
+          <div className="text-end w-full h-fit flex">
+            <span className="w-11/12 mx-10">aa</span>
+            <div alt='img' className="w-1/12 h-16 bg-slate-50" />
+            {/* <Image src={janimal} alt='img' className="w-1/12 h-16" /> */}
+          </div>
+        </div>
+        <div className="w-full flex h-20">
+          <textarea className="bg-red-200 w-full overflow-scroll overflow-x-hidden"></textarea>
+          <button className="bg-red-800 w-32">send</button>
+        </div>
+      </div>
     </main>
   );
+}
+function Send() {
+  return (
+    <div>aa</div>
+  )
 }
